41c2ef786318411b4efca1800aa4f82f8a075f38
HungryDoctor <assassin_dok@yahoo.com>
2014-11-23 19:42:19 +0200

    First commit, file chunking

---
+++ File_Transferring/Controller.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Transferring
+{
+    class Controller
+    {
+        public Controller(Form1 form)
+        {
+            this.form = form;
+        }
+
+        Form1 form;
+        Thread readFile;
+        string fileName;
+        const int chunk = 60 * 1024;
+        long counter = 0;
+
+        public void OpenFile()
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = dialog.FileName;
+                    ResetThread();
+                    readFile.Start();
+                }
+            }
+        }
+
+        void ResetThread()
+        {
+            readFile = new Thread(ReadAndProcessLargeFile);
+        }
+
+
+        void ReadAndProcessLargeFile()
+        {
+            FileStream fileStram = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            using (fileStram)
+            {
+                byte[] buffer = new byte[chunk];
+                fileStram.Seek(0, SeekOrigin.Begin);
+                int bytesRead = fileStram.Read(buffer, 0, chunk);
+                while (bytesRead > 0)
+                {
+                    ProcessChunk(buffer, bytesRead);
+                    bytesRead = fileStram.Read(buffer, 0, chunk);
+                }
+            }
+            MessageBox.Show(counter.ToString());
+        }
+
+        void ProcessChunk(byte[] buffer, int bytesRead)
+        {
+            // Do the processing here
+            counter++;
+        }
+
+    }
+}
---
+++ File_Transferring/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Transferring
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            controller = new Controller(this);
+        }
+
+        Controller controller;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            controller.OpenFile();
+        }
+    }
+}
